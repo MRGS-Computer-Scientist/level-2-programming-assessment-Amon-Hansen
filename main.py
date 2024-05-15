9a481fce894eed93c3895a0dd37b1cd49a8e1db0
Amon-Hansen <21092@students.mrgs.com>
2024-05-15 14:55:36 +1200

    Created window and frame with labels

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+w_width = 500
+w_height = 700
+
+window = Tk()
+window. geometry(str(w_width) + "x" +str(w_height))
+window. title("My App")
+
+main_frame = Frame(background="orange", width=w_width, height=w_height)        
+main_frame.pack()
+
+hello_label = Label (text="Home!")
+hello_label.place (x=20, y=20)
+
+hello_label = Label (text="Sports")
+hello_label.place (x=70, y=20)
+
+hello_label = Label (text="Help")
+hello_label.place (x=450, y=20)
+
+window.mainloop() 
