a324a7ab6b72d2cd89aafa082358c19d88b725f1
Amon-Hansen <21092@students.mrgs.com>
2024-05-28 09:54:18 +1200

    made true or false

--- App.py
+++ App.py
@@ -1,33 +1,38 @@
 from tkinter import *
 from app_settings import *
+from os import *
 
 class App():
 
-    def _init_(self):
-        w_width = 500
-        w_height = 700
+    
+    def __init__(self):
+        self.window = Tk()
+        self.window.geometry(str(w_width) + "x" + str(w_height))
+        self.window.title(app_title)
 
-        bg_color = "#E7DFF"
+        self.top_frame = Frame(background='red', width=w_width, height=100)
+        self.top_frame.pack()
 
-        window = Tk()
-        window. geometry(str(w_width) + "x" +str(w_height))
-        window. title("My App")
+        self.main_frame = Frame(background=bg_color, width=w_width, height=(w_height-200))
+        self.main_frame.pack()
 
-        top_frame = Frame(background='black', width=w_width, height=100)
-        top_frame.pack()    
+        self.bottom_frame = Frame(background='blue', width=w_width, height=100)
+        self.bottom_frame.pack(side='bottom')
 
-        main_frame = Frame(background="orange", width=w_width, height=w_height)        
-        main_frame.pack()
+        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='green')
+        self.home_button.place(x=0,y=0)
 
-        hello_label = Label (text="Home!")
-        hello_label.place (x=20, y=20)
+        self.exit_button = Button(self.bottom_frame, text="Exit", height=2, width=5, bg='green', command=exit)
+        self.exit_button.place(x=100,y=0)
 
-        hello_label = Label (text="Sports")
-        hello_label.place (x=70, y=20)
+        self.dirname = path.dirname(__file__)
+        self.filename = path.join(self.dirname, 'images/')
 
-        hello_label = Label (text="Help")
-        hello_label.place (x=450, y=20)
+        print("The path is", self.filename)
 
-        home_button
+        self.window.mainloop()
 
-        window.mainloop() 
+
+    def exit(self):
+        self.window.destroy()
+
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from App import *
 
 
 
-if "__name__"=="__main__":
+if __name__=="__main__":
+    app = App()
